ef0155f901fe0d18e237888950d34a584a098fe3
Carolina Oliveira <carolinaoliveira@id.uff.br>
2020-08-11 20:47:11 -0300

    Criado atributo complexo filhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,16 +1,19 @@
 class Pessoa:
-    def __init__(self, nome = None, idade = 29):
+    def __init__(self, *filhos, nome = None, idade = 29):
         self.idade = idade
         self.nome = nome
+        self.filhos = list(filhos)
 
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 if __name__ == '__main__':
-    p = Pessoa('Carolina')
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-    print(p.nome)
-    p.nome = 'Carol'
-    print(p.nome)
+    cristina = Pessoa(nome ='Cristina')
+    carolina = Pessoa(cristina, nome ='Carolina')
+    print(Pessoa.cumprimentar(carolina))
+    print(id(carolina))
+    print(carolina.cumprimentar())
+    print(carolina.nome)
+    print(carolina.idade)
+    for filho in carolina.filhos:
+        print(filho.nome)
